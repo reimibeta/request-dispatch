1c7200e3a9677680664935bb72c6390735d7aee8
reimi <65539218+reimibeta@users.noreply.github.com>
2023-06-04 11:57:15 +0700

    .

--- dist/request/request.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const request: {
-    put: <T>(url: string, params: any, props: import("./interfaces/request.interface").RequestInterface<T>) => void;
-    post: <T_1>(url: string, params?: any, props?: import("./interfaces/request.interface").RequestInterface<T_1> | undefined) => void;
-    get: <T_2>(url: string, props: import("./interfaces/request.interface").RequestInterface<T_2>) => void;
-};

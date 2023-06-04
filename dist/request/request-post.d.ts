becfaa5fc488bda75ceeeccae03aadd9cef1a8b1
reimi <65539218+reimibeta@users.noreply.github.com>
2023-06-04 11:43:18 +0700

    .

--- dist/request/request-post.d.ts
+++ dist/request/request-post.d.ts
@@ -1,2 +1,2 @@
 import { RequestInterface } from "./interfaces/request.interface";
-export declare const post: <T>(url: string, params?: any, props?: RequestInterface<T> | undefined) => void;
+export declare const post: <T>(url: string, params?: any, props?: RequestInterface<T>) => void;
--- src/request/request.ts
+++
@@ -1,9 +0,0 @@
-import { post } from "./request-post";
-import { put } from "./request-put";
-import { get } from "./request-get";
-
-export const request = {
-    put,
-    post,
-    get,
-} 
